2fdb1ab492a2956a1d41e8892a53f2dd2b743ff0
Martin Pollard <mp15@sanger.ac.uk>
2012-12-04 14:01:44 +0000

    Tweak bam_strip_tags

--- modules/VRPipe/Steps/bam_strip_tags.pm
+++ modules/VRPipe/Steps/bam_strip_tags.pm
@@ -98,9 +98,13 @@ class VRPipe::Steps::bam_strip_tags with VRPipe::StepRole {
         my $out_bam_path = $out_bam->path->stringify;
         $in_bam->disconnect;
         my $num_records = 0;
+        
+        $bp->get_fields('FLAG');
+        my $parsed_record = $pars->parsed_record();
+
         while ($bp->next_record) {
             $bp->write_result($out_bam_path);
-            $num_records++;
+            if ($bp->passes_qc($parsed_record->{FLAG})) $num_records++;
         }
         $bp->close;
         
